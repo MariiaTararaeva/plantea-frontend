9a04ad7a3cd332c3b29334edc5b5f656a451f054
Lena Cortes <lena.cortes@usal.es>
2025-01-30 00:11:24 +0100

    random plants refresh button added

--- src/pages/RandomPlantsPage.jsx
+++ src/pages/RandomPlantsPage.jsx
@@ -4,26 +4,35 @@ import { useNavigate } from "react-router-dom";
 const RandomPlantsPage = () => {
     const navigate = useNavigate();
     const [plants, setPlants] = useState([]); // Random plants for homepage
+
+    //navigating to a plant details page
     const handlePlantClick = (plantId) => {
         navigate(`/plants/${plantId}`);
     };
+
+    //function to fetch the plants:
+    const fetchRandomPlants = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/plants/random?limit=20`); // Assuming this endpoint exists
+            if (!response.ok) throw new Error("Failed to fetch plants");
+            const data = await response.json();
+            setPlants(data);
+        } catch (error) {
+            console.error("Error fetching random plants:", error);
+        }
+    };
+
+    //getting 20 on mounting
     useEffect(() => {
-        const fetchRandomPlants = async () => {
-            try {
-                const response = await fetch(`${import.meta.env.VITE_API_URL}/api/plants/random?limit=20`); // Assuming this endpoint exists
-                if (!response.ok) throw new Error("Failed to fetch plants");
-                const data = await response.json();
-                setPlants(data);
-            } catch (error) {
-                console.error("Error fetching random plants:", error);
-            }
-        };
         fetchRandomPlants();
     }, []);
+
+
     return (
         <>
             <div className="random-plants">
                 <h2>Explore Plants</h2>
+                <button onClick={fetchRandomPlants}>Refresh</button>
                 <div className="plant-grid">
                     {plants.map((plant) => (
                         <div key={plant._id} className="plant-card" onClick={() => handlePlantClick(plant._id)}>
@@ -33,6 +42,7 @@ const RandomPlantsPage = () => {
                                 className="plant-image"
                             />
                             <p>{plant.common_name || plant.scientific_name}</p>
+                            <p>{plant.watering === "Average" ? "💧💧" : plant.watering === "Frequent" ? "💧💧💧" : plant.watering === "Minimum" ? "💧" : null}</p>
                         </div>
                     ))}
                 </div>
